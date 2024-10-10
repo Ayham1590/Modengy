0fc3fc4bd4ee710a6a71c2f668d9b90d862d7937
Ayham1590 <2107020014@students.stamford.edu>
2024-10-10 09:27:47 +0700

    edit 1

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ function copyToClipboard() {
         alert("Copied the CA: " + copyText);
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselContent = document.getElementById('carousel-content');
+    const speed = 1;
+
+    carouselContent.innerHTML += carouselContent.innerHTML;
+
+    let currentPosition = 0;
+
+    function scrollCarousel() {
+        currentPosition -= speed;
+
+        if (Math.abs(currentPosition) >= carouselContent.scrollWidth / 2) {
+            currentPosition = 0;
+        }
+
+        carouselContent.style.transform = `translateX(${currentPosition}px)`;
+        requestAnimationFrame(scrollCarousel);
+    }
+
+    scrollCarousel();
+});
+
+window.addEventListener('scroll', function () {
+    const parallaxSection = document.querySelector('.parallax-image');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) {
+        parallaxSection.classList.add('scrolled');
+    } else {
+        parallaxSection.classList.remove('scrolled');
+    }
+});
